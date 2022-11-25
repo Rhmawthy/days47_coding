5091ea77299bd5ac4fb68af3ef9c2967d90937a2
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-25 20:01:47 +0800

    Add files via upload

---
+++ days46_fungsi.py
@@ -0,0 +1,8 @@
+angka1=int(input("masukkan angka1 :"))
+angka2=int(input("masukkan angka2 :"))
+
+def menukar(y,x):
+	print("nilai yang belum tertukar",y)
+	print("nilai yang belum tertukar",x)
+	
+menukar ( y=(angka1,angka2),  x=(angka2,angka1))
